c3a3cccd81a54b3038b2e3ee9fc8eca478879322
Jose Ortega Valiente <zackreynolt@gmail.com>
2016-04-08 15:15:15 +0200

    Code for 2 basic scripts: Destroy by Boundary and the first Enemy's Controller

---
+++ Assets/_Scripts/e1GoblinController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class e1GoblinController : MonoBehaviour {
+
+	public float speed;
+	public float gravity = 20.0F;
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update () {
+		moveDirection = new Vector3(-speed, 0, 0);
+		moveDirection.y -= gravity * Time.deltaTime;
+		transform.Translate (moveDirection * Time.deltaTime);
+	}
+}
