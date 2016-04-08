f31b401119976122eb22315417bd136350ce8192
JavierPin <javierpinels@gmail.com>
2016-04-08 13:24:07 +0200

    Pequeños rotoques y pruebas. Añadido enemigo basico SacoBoxeo

---
+++ Assets/_Scripts/Spawn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    public GameObject prefab, posicion;
+	void Start () {
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Instantiate(prefab, posicion.transform.position, Quaternion.Euler(posicion.transform.eulerAngles));
+    }
+}
