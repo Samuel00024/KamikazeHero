3f8897642b6c2804d877db8a66e75426b322b12f
Jose Ortega Valiente <zackreynolt@gmail.com>
2016-05-06 14:03:44 +0200

    GoblinController modification, GameController displays time playing in the actual level

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public Text timeText;
 
+	void Start () {
+		timeText.text = "Time: 00:00";
 	}
 		
+	void Update() {
+		//=== Time Measurement =====================
+		timeText.text = "Time: " + Time.timeSinceLevelLoad.ToString("F2");
+
+	}
+
 }
--- Assets/_Scripts/e1GoblinController.cs
+++ Assets/_Scripts/e1GoblinController.cs
@@ -3,14 +3,29 @@ using System.Collections;
 
 public class e1GoblinController : MonoBehaviour {
 
-	public float speed;
-	public float gravity = 20.0F;
+	private float speed = 5;
+	private int damage = 1;
 
+	[Header("Parameters")]
+	public float gravity = 10.0F;
+
+	[Header("References")]
+	public Transform Player;
+
+	private GameController gameController;
 	private Vector3 moveDirection = Vector3.zero;
 
-	void Update () {
-		moveDirection = new Vector3(-speed, 0, 0);
-		moveDirection.y -= gravity * Time.deltaTime;
-		transform.Translate (moveDirection * Time.deltaTime);
+
+	void Start () {
+
 	}
+
+
+	void Update () {
+		//moveDirection = new Vector3(Player.velocity.x, 0, Player.velocity.z); //Player velocity so we know the direction
+		moveDirection = new Vector3((-1)*Player.forward.x, 0, (-1)*Player.forward.z);
+		moveDirection.y -= gravity * Time.deltaTime;
+		transform.Translate (moveDirection * speed  * Time.deltaTime);
+	}
+		
 }
