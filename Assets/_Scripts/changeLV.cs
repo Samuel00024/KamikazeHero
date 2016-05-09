8021bbbb426f7cd0733eb5d98ca70a581d5dbf9c
JavierPin <javierpinels@gmail.com>
2016-05-09 13:24:36 +0200

    Subiendo archivos faltantes

---
+++ Assets/_Scripts/changeLV.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class changeLV : MonoBehaviour {
+
+    public int nextLV;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        new WaitForSeconds(5);
+        SceneManager.LoadScene(nextLV);
+
+    }
+
+}
